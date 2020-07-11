e18342cd9dc8c87928e61ea78b057a764d334819
Aaron Collins <aa.collins.95@gmail.com>
2020-07-11 17:42:11 -0400

    18 components example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -7,31 +9,27 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters:[
-        {
-          name: 'Frankenstein',
-          id: 'asc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'awc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'dfw3'
-        }
-      ]
+      monsters:[]
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
   }
 
   render() {
     return (
       <div className="App">
+      <CardList name="Baller" > 
         {
           this.state.monsters.map( 
             monster => <h1 key={monster.id}> {monster.name} </h1> 
           )
         }
+      </CardList>
+        
       </div>
     );
   }
