c26388928052df5abbb16bf7d04143e377225850
Aaron Collins <aa.collins.95@gmail.com>
2020-07-12 12:17:02 -0400

    Updates

---
+++ precache-manifest.9c2cc5644a17257c38278ed0eec6f3c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5923c3e50f284dd35ffb523358aa51",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d2aa062fd71ad8a5fee9",
+    "url": "/monsters-rolodex/static/css/main.9bb96597.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aa062fd71ad8a5fee9",
+    "url": "/monsters-rolodex/static/js/main.af1688b3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
